59d993fb08e1d8e28c65f88ca972250a4929fc30
Adriaan Wisse <adriaan.wisse@trifork.nl>
2021-01-17 20:40:49 +0100

    GPIO test script

---
+++ Test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+
+print('Press Control + C to exit the program')
+
+GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
+GPIO.setwarnings(False)
+
+ledPin = 12
+relayPin = 11
+
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(relayPin, GPIO.OUT)
+
+
+def flash_light(amount):
+    print('Test LED by flashing.')
+    for x in range(amount):
+
+        GPIO.output(ledPin, GPIO.HIGH)
+        time.sleep(.5)
+        GPIO.output(ledPin, GPIO.LOW)
+
+
+def switch_relay():
+    print('Test relay by opening and closing.')
+    GPIO.output(relayPin, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(relayPin, GPIO.HIGH)
+
+
+try:
+    while True:
+        flash_light(5)
+        switch_relay()
+
+except KeyboardInterrupt:  # Stops program when "Control + C" is entered
+    GPIO.cleanup()  # Turns OFF everything
