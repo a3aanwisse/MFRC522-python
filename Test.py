167bf4088f71a7675cb2d4c082d58082b2456ab9
Adriaan Wisse <adriaan.wisse@trifork.nl>
2021-01-17 21:12:16 +0100

    GPIO test script - using gpiozero

--- Test.py
+++ Test.py
@@ -1,22 +1,33 @@
 #!/usr/bin/env python
 
-from gpiozero.pins.rpigpio import RPiGPIOFactory
 from gpiozero import LED
+from gpiozero import OutputDevice
 import time
+import sys
 
 print('Press Control + C to exit the program')
 
-ledPin = 18
-led = LED(ledPin)
+LED_PIN = 18
+RELAY_PIN = 17
 
-# GPIO.setmode(GPIO.BOARD)  # the pin numbers refer to the board connector not the chip
-# GPIO.setwarnings(False)
+led = LED(LED_PIN)
+relay = OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
 
 
-# relayPin = 11
+def set_relay(status):
+    if status:
+        print("Setting relay: ON")
+        relay.on()
+    else:
+        print("Setting relay: OFF")
+        relay.off()
 
-# GPIO.setup(ledPin, GPIO.OUT)
-# GPIO.setup(relayPin, GPIO.OUT)
+
+def toggle_relay():
+    print("Toggling relay")
+    relay.toggle()
+    time.sleep(.5)
+    relay.toggle()
 
 
 def flash_light(amount):
@@ -30,18 +41,12 @@ def flash_light(amount):
         time.sleep(.25)
 
 
-# def switch_relay():
-#     print('Test Relay by opening and closing.')
-#     GPIO.output(relayPin, GPIO.LOW)
-#     time.sleep(.5)
-#     GPIO.output(relayPin, GPIO.HIGH)
-#     time.sleep(.5)
-
-
 try:
-    while True:
+    set_relay(False)
+    while 1:
         flash_light(5)
-        # switch_relay()
+        toggle_relay()
 
 except KeyboardInterrupt:  # Stops program when "Control + C" is entered
-    GPIO.cleanup()  # Turns OFF everything
+    set_relay(False)
+    sys.exit(0)
