f9b7d4f4bf8e805f1eeae0b6e70d9dec83bbe474
Adriaan Wisse <adriaan.wisse@trifork.nl>
2021-01-24 10:39:02 +0100

    Integrated in website

--- controller.py
+++ controller.py
@@ -21,7 +21,7 @@
 #    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-import MFRC522
+# import MFRC522
 import time
 import signal
 from gpiozero import LED
@@ -35,22 +35,14 @@ led = None
 relay = None
 allowed = [[8, 155, 225, 64, 50], [7, 155, 107, 64, 183], [54, 175, 183, 66, 108]]
 # Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+# MIFAREReader = MFRC522.MFRC522()
 
 
 def setup():
     global led, relay
     led = LED(LED_PIN)
     relay = OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
-    signal.signal(signal.SIGINT, end_read)
     # start_listening()
-
-
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(received_signal, frame):
-    # global continue_reading
-    print("Ctrl+C captured, ending read.")
-    # continue_reading = False
 
 
 def switch_led_on():
@@ -70,49 +62,49 @@ def toggle_relay():
     relay.toggle()
 
 
-def start_listening():
-    # This loop keeps checking for chips. If one is near it will get the UID and authenticate
-    while continue_reading:
-
-        # Scan for cards
-        (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
-
-        # If a card is found
-        if status == MIFAREReader.MI_OK:
-            print("Card detected")
-
-        # Get the UID of the card
-        (status, uid) = MIFAREReader.MFRC522_Anticoll()
-
-        # If we have the UID, continue
-        if status == MIFAREReader.MI_OK:
-
-            # Print UID
-            print("Card read UID:")
-            print(uid)
-
-            # This is the default key for authentication
-            key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
-
-            # Select the scanned tag
-            MIFAREReader.MFRC522_SelectTag(uid)
-
-            # Authenticate
-            status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-            # Check if authenticated
-            if status == MIFAREReader.MI_OK:
-                MIFAREReader.MFRC522_Read(8)
-                MIFAREReader.MFRC522_StopCrypto1()
-
-                if uid in allowed:
-                    switch_led_on()
-                    toggle_relay()
-                    time.sleep(10)
-                    switch_led_off()
-
-                    print("ENTRANCE!")
-                else:
-                    print("BLOCKED!")
-            else:
-                print("Authentication error")
+# def start_listening():
+#     # This loop keeps checking for chips. If one is near it will get the UID and authenticate
+#     while continue_reading:
+#
+#         # Scan for cards
+#         (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+#
+#         # If a card is found
+#         if status == MIFAREReader.MI_OK:
+#             print("Card detected")
+#
+#         # Get the UID of the card
+#         (status, uid) = MIFAREReader.MFRC522_Anticoll()
+#
+#         # If we have the UID, continue
+#         if status == MIFAREReader.MI_OK:
+#
+#             # Print UID
+#             print("Card read UID:")
+#             print(uid)
+#
+#             # This is the default key for authentication
+#             key = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+#
+#             # Select the scanned tag
+#             MIFAREReader.MFRC522_SelectTag(uid)
+#
+#             # Authenticate
+#             status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+#
+#             # Check if authenticated
+#             if status == MIFAREReader.MI_OK:
+#                 MIFAREReader.MFRC522_Read(8)
+#                 MIFAREReader.MFRC522_StopCrypto1()
+#
+#                 if uid in allowed:
+#                     switch_led_on()
+#                     toggle_relay()
+#                     time.sleep(10)
+#                     switch_led_off()
+#
+#                     print("ENTRANCE!")
+#                 else:
+#                     print("BLOCKED!")
+#             else:
+#                 print("Authentication error")
