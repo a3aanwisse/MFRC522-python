f3f57463cd6cb5a36224971babcd4cf713b322bf
adriaan <a3aan.wisse@gmail.com>
2022-12-27 15:15:13 +0100

    Update test page

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def show_card_ids():
     return render_template('cards.html', data=controller.get_allowed_card_ids())
 
 
-@app.route('/cards/<card_id>')
+@app.route('/cards/<card_id>', methods=['PUT'])
 def add_new_card_id(card_id):
     controller.add_allowed_card_id(card_id)
     return render_template('newId.html', id=card_id)
@@ -25,23 +25,23 @@ def test():
     return render_template('test.html')
 
 
-@app.route("/relay/toggle", methods=['PUT'])
+@app.route('/relay/toggle', methods=['PUT'])
 def toggle_relay():
     controller.toggle_relay()
     return 'ok', 204
 
 
-@app.route("/reed/closed-door", methods=['PUT'])
+@app.route('/reed/closed-door', methods=['PUT'])
 def read_reed_closed_door():
     return controller.read_reed_closed_door()
 
 
-@app.route("/reed/open-door", methods=['PUT'])
+@app.route('/reed/open-door', methods=['PUT'])
 def read_reed_open_door():
     return controller.read_reed_open_door()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     threading.Thread(target=lambda: app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)).start()
     controller.setup()
     controller.start_listening()
