bbef14c26466e2939bf2b055bc274420ba0adf35
AdriaanWisse <adriaan.wisse@group9.nl>
2026-01-26 22:26:54 +0100

    Experiments

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # launcher.sh
-export VALID_CARDS_FILE_PATH="/home/valid_card_ids_2.txt"
+export VALID_CARDS_FILE_PATH="/home/pi/valid_card_ids_2.txt"
 
 cd /
 cd home/pi/MFRC522-python || exit
