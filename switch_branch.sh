3aa0dd7712285c78164f80c59f5a9656bacf8494
AdriaanWisse <adriaan.wisse@group9.nl>
2026-03-16 22:09:22 +0100

    Add script to be able to switch the branch

--- switch_branch.sh
+++ switch_branch.sh
@@ -6,8 +6,10 @@ set -e
 
 # Stop het draaiende Python-script
 echo "Stoppen van het Python-script 'python app.py'..."
-pkill -f "python app.py"
+pkill -f "python3 app.py"
 
+echo "Switch branch naar experiments-2"
 git checkout experiments-2
 
-./launcher.sh
+echo "Launch!"
+/home/pi/MFRC522-python/launcher.sh >/home/pi/data/dooropener/logs/dooropener 2>&1
