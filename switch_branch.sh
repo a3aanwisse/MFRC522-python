02e4f10c94a708fc8ca5251115f6fec7b4b8debb
AdriaanWisse <adriaan.wisse@group9.nl>
2026-03-16 22:10:24 +0100

    Add script to be able to switch the branch

--- switch_branch.sh
+++ switch_branch.sh
@@ -5,8 +5,8 @@
 set -e
 
 # Stop het draaiende Python-script
-echo "Stoppen van het Python-script 'python app.py'..."
-pkill -f "python3 app.py"
+echo "Stoppen van het Python-script 'python3 app.py'..."
+pkill -f "python3 app.py" || true
 
 echo "Switch branch naar experiments-2"
 git checkout experiments-2
